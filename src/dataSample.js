461dc00a0b149fce58956d145510f2cfcbd23f1b
unknown <mla.gajic@gmail.com>
2019-11-05 01:13:27 +0100

    Adding column.json and dataSample.js

---
+++ src/dataSample.js
@@ -0,0 +1,8 @@
+const dataSample = [
+    {id: 1, full_name: "Mladen Gajic", height: 1.8328, date: "04.02.2016.", description: "Web designer", email: "mla.gajic@gmail.com"},
+    {id: 2, full_name: "Aleksandar Milosevic", height: 1.8828, date: "10.04.2017.", description: "Pićkin dim junior developer",email: "aco@gmail.com"},
+    {id: 3, full_name: "Dejan Tripic", height: 1.8228, date: "04.09.2015.", description: "Web designer",email: "tripa@gmail.com"},
+    {id: 4, full_name: "Aleksa Šarov", height: 1.8398, date: "25.06.2018.", description: "Pićkin dim midijor developer",email: "aleksa@gmail.com"}
+];
+
+export default dataSample;
